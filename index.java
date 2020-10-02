c5266186490ad645b614270dba0be0802d02e9e5
MaxBromley1 <mjbro2003@gmail.com>
2020-10-02 14:36:55 +0000

    here are my shapes

--- index.java
+++ index.java
@@ -1,13 +1,24 @@
 void setup() {
 	size(300, 300);
-}
-
-void draw(){
-    
-
-
+  
 
 
 }
 
+
+
+void draw(){  ellipseMode(CENTER);
+    background(200);
+  rectMode(CENTER);
+ 
+  fill(255,mouseX,mouseY)
+    rect(mouseX, mouseY,mouseX,mouseY);
+  
+   fill(mouseX,mouseY,255)
+     ellipse(120, 110,mouseX,mouseY);
+}
+
+void mousePressed(){
+  Println ("Color Madness");
+}
 //dont forget to add mouse and keypressed interaction functions
